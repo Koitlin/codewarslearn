6b4774d13e07d5bdb45607c75ca8f8e3a3ac22f9
Eugene Kotygin <kotigin@yandex.ru>
2024-10-08 21:15:06 +0300

    refactoring

--- RegexValidatePINCode.js
+++ RegexValidatePINCode.js
@@ -1,7 +1,3 @@
 function validatePIN(pin) {
-    if (/^\d{4}$/.test(pin) || /^\d{6}$/.test(pin)) {
-        return true
-    } else {
-        return false
-    }
+    return /^\d{4}$/.test(pin) || /^\d{6}$/.test(pin)
 }
