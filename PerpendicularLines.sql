0212d642318225b02d8d89cfc6ce661c1119cc3f
Eugene Kotygin <kotigin@yandex.ru>
2024-10-09 22:04:45 +0300

    PerpendicularLines_Done

---
+++ PerpendicularLines.sql
@@ -0,0 +1,6 @@
+--# write your SQL statement here:
+--you are given a table 'perpendicular' with column 'n'
+-- return a table with this column and your result in a column named 'res'.
+select n, (
+    (n / 2) * (n - n / 2)
+) as res from perpendicular
