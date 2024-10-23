2a41d5185bfc7d6a9631aff63c650d8c7c358809
Eugene Kotygin <ekotygin@gmail.com>
2024-10-23 20:36:33 +0300

    Count by X SQL Done

---
+++ Count by X.sql
@@ -0,0 +1,16 @@
+create or replace function get_seq(x integer, n integer)
+returns integer[] as $$
+declare
+  i integer;
+  result integer[];
+begin
+  for i in 1..n loop
+    result[i] = x * i;
+  end loop;
+  return result;
+end;
+$$ language plpgsql;
+
+select x, n, get_seq(x, n) as res
+from counter
+order by x, n
